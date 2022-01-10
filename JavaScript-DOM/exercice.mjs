3a30377350992b76d9cd7f7a5243292222941d4c
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2022-01-10 14:42:36 +0100

    Correction

--- JavaScript-DOM/exercice.mjs
+++ JavaScript-DOM/exercice.mjs
@@ -5,13 +5,42 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-// Enoncé slide 71
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 function jouer() {
+  if (essais.length) {
+    console.log('Vous avez déjà joué : ' + essais.join(', '))
+  }
+
   rl.question('Quel est le nombre ? ', (answer) => {
-    console.log(answer);
-    jouer();
+    const entierSaisi = Number(answer);
+
+    if (Number.isNaN(entierSaisi)) {
+      console.log('Il faut saisir un nombre');
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      jouer();
+    } else if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      jouer();
+    } else {
+      console.log('Gagné');
+      rl.close();
+    }
+
+
   });
 }
 
+const entierAlea = getRandomInt(0, 100);
+const essais = [];
 jouer();
